4f1862ada3e29b33eb64bc0290eb3206a618f75b
ArtemMasterJava <aanohin1@gmail.com>
2016-07-09 00:07:22 +0300

    added logo and menu but method onClick dosen't work(

---
+++ app/src/main/java/com/ivart/helptodecide/menu_items/MyDecide.java
@@ -0,0 +1,17 @@
+package com.ivart.helptodecide.menu_items;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ivart.helptodecide.R;
+
+/**
+ * Created by Artem on 08.07.2016.
+ */
+public class MyDecide extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_decide);
+    }
+}
