1f3f7999c453c17f160bb59f67e82febf9904067
Ivan Kyselov <kyselovivan@gmail.com>
2016-07-31 17:26:45 +0300

    Remove methods onClick, onClick1, onClick2 from Menu_Activity

--- app/src/main/java/com/ivart/helptodecide/MainActivity.java
+++ app/src/main/java/com/ivart/helptodecide/MainActivity.java
@@ -1,13 +1,34 @@
 package com.ivart.helptodecide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.ivart.helptodecide.menu_items.About;
+import com.ivart.helptodecide.menu_items.Decide;
+import com.ivart.helptodecide.menu_items.MyDecide;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, Decide.class);
+        startActivity(intent);
+    }
+
+    public void onClick1(View view) {
+        Intent intent = new Intent(MainActivity.this, MyDecide.class);
+        startActivity(intent);
+    }
+
+    public void onClick2(View view) {
+        Intent intent = new Intent(MainActivity.this, About.class);
+        startActivity(intent);
     }
 }
