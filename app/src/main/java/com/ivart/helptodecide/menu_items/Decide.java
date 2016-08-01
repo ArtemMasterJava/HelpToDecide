8db56b99a88b3c0f216b16296f1839df2cb58e37
Ivan Kyselov <kyselovivan@gmail.com>
2016-08-01 16:35:00 +0300

    refactor onClickListener

--- app/src/main/java/com/ivart/helptodecide/menu_items/Decide.java
+++ app/src/main/java/com/ivart/helptodecide/menu_items/Decide.java
@@ -14,7 +14,7 @@ import com.ivart.helptodecide.R;
 /**
  * Created by Artem on 08.07.2016.
  */
-public class Decide extends Activity {
+public class Decide extends Activity implements View.OnClickListener {
 
     public EditText descriptionTitile;
     public Button willIfItHappens;
@@ -27,48 +27,39 @@ public class Decide extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.go_to_decide_layout);
 
-        descriptionTitile = (EditText)findViewById(R.id.descisionTitle);
-        willIfItHappens = (Button)findViewById(R.id.btn_will_if_it_happens);
-        willIfItDoesnt = (Button)findViewById(R.id.btn_will_if_it_doesnt);
-        wontIfItHappens = (Button)findViewById(R.id.btn_wont_if_it_happens);
-        wontIfItDoesnt = (Button)findViewById(R.id.btn_wont_if_it_doesnt);
+        descriptionTitile = (EditText) findViewById(R.id.descisionTitle);
+        willIfItHappens = (Button) findViewById(R.id.btn_will_if_it_happens);
+        willIfItDoesnt = (Button) findViewById(R.id.btn_will_if_it_doesnt);
+        wontIfItHappens = (Button) findViewById(R.id.btn_wont_if_it_happens);
+        wontIfItDoesnt = (Button) findViewById(R.id.btn_wont_if_it_doesnt);
 
-        willIfItHappens.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Decide.this, Description.class);
-                intent.putExtra("Square_0",0);
-                startActivity(intent);
-            }
-        });
-
-        willIfItDoesnt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Decide.this, Description.class);
-                intent.putExtra("Square_0",1);
-                startActivity(intent);
-            }
-        });
-
-        wontIfItHappens.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Decide.this, Description.class);
-                intent.putExtra("Square_0",2);
-                startActivity(intent);
-            }
-        });
-
-        wontIfItDoesnt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Decide.this, Description.class);
-                intent.putExtra("Square_0",3);
-                startActivity(intent);
-            }
-        });
+        willIfItHappens.setOnClickListener(this);
+        willIfItDoesnt.setOnClickListener(this);
+        wontIfItHappens.setOnClickListener(this);
+        wontIfItDoesnt.setOnClickListener(this);
     }
 
+    public void startDescriptionActivity(int squareNumber) {
+        Intent intent = new Intent(Decide.this, Description.class);
+        intent.putExtra("Square_0", squareNumber);
+        startActivity(intent);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_will_if_it_happens:
+                startDescriptionActivity(0); break;
+            case R.id.btn_will_if_it_doesnt:
+                startDescriptionActivity(1); break;
+            case R.id.btn_wont_if_it_happens:
+                startDescriptionActivity(2); break;
+            case R.id.btn_wont_if_it_doesnt:
+                startDescriptionActivity(3); break;
+        }
+    }
 }
+
+
+
+
