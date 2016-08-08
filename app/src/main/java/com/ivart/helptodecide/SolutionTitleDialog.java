20d4c42cb42a6dd5e992109ed73a2af5ee6287ae
Ivan Kyselov <kyselovivan@gmail.com>
2016-08-08 12:44:44 +0300

    SolutionTitleDialog class added

---
+++ app/src/main/java/com/ivart/helptodecide/SolutionTitleDialog.java
@@ -0,0 +1,38 @@
+package com.ivart.helptodecide;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ivart.helptodecide.menu_items.Decide;
+
+public class SolutionTitleDialog extends Activity {
+
+    Button createSolution;
+    EditText solutionName;
+    DbHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solution_title_dialog);
+        solutionName = (EditText) findViewById(R.id.solution_title);
+        createSolution = (Button) findViewById(R.id.create_solution);
+        dbHelper = new DbHelper(this);
+
+        createSolution.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SolutionTitleDialog.this, Decide.class);
+                String name = solutionName.getText().toString();
+                long id = dbHelper.createSolution(new Solution(name));
+                intent.putExtra("solutionId",id);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
